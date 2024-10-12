f7c454f865d9ed20ef07b5687d6e6c1c75434507
HarshRajShorthillsAI <harsh.raj@shorthills.ai>
2024-10-12 16:05:33 +0530

    variable updated

--- Tutorial/Variables/variables.py
+++ Tutorial/Variables/variables.py
@@ -10,3 +10,19 @@ print(type(y))
 
 x = "Sally" # x is now of type str
 print(x)
+
+a, b, c = 1, 2, "Three"
+print(a, b, c)
+
+def my_function():
+    global t
+    t = 3
+    u = 10  # Local variable
+    print(u)
+
+my_function()  # Output: 10
+
+print(t)
+
+del x
+print(x)
