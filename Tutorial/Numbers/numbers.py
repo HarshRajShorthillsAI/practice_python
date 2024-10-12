5f074b75c3b7cacd4a084b98f7813a0fb5243cf9
HarshRajShorthillsAI <harsh.raj@shorthills.ai>
2024-10-12 15:47:38 +0530

    Commited for Variables

---
+++ Tutorial/Numbers/numbers.py
@@ -0,0 +1,11 @@
+# x = 35e3
+# y = 12E4
+# z = -87.7e100
+
+# print(type(x))
+# print(type(y))
+# print(type(z))
+
+import random
+
+print(random.randrange(1, 10)) 
---
+++ Tutorial/Variables/variables.py
@@ -0,0 +1,12 @@
+x = 5
+y = "John"
+print(x)
+
+print(type(x))
+
+print(y)
+
+print(type(y)) 
+
+x = "Sally" # x is now of type str
+print(x)
