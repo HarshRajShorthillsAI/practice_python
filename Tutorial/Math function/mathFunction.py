a8d508ac6bf306af7f0de69930aced0912466ce8
HarshRajShorthillsAI <harsh.raj@shorthills.ai>
2024-10-12 10:30:22 +0530

    Commited for Math functions

---
+++ Tutorial/Math function/mathFunction.py
@@ -0,0 +1,46 @@
+import math
+
+# Basic functions
+print("math.ceil(4.3) = ",math.ceil(4.3))
+print("math.floor(4.7) = ",math.floor(4.7))
+print("math.fabs(-10.5) = ",math.fabs(-10.5))
+
+# Trigonometric functions
+print("math.sin(math.pi/2) = ",math.sin(math.pi/2))
+print("math.cos(0) = ",math.cos(0))
+
+# Logarithmic and exponential functions
+print("math.exp(2) = ",math.exp(2))
+print("math.log(10) = ",math.log(10))
+print("math.log10(100) = ",math.log10(100))
+
+# Power and roots
+print("math.sqrt(16) = ",math.sqrt(16))
+print("math.pow(2, 3) = ",math.pow(2, 3))
+
+# Constants
+print("math.pi = ",math.pi)
+print("math.e = ",math.e)
+
+# combination
+print("math.comb(5, 2) = ",math.comb(5, 2))
+print("math.perm(10, 4) = ",math.perm(10, 4))
+
+
+#miscellaneous
+print("math.factorial(4) = ",math.factorial(4))
+print("math.copysign(4, -3) = ",math.copysign(4, -3))
+print("math.prod([19, 3, 4]) = ",math.prod([19, 3, 4]))
+
+#Eucledian distance
+print("math.dist([0,3], [4,0]) = ",math.dist([0,3], [4,0]))
+
+#GCD
+print("math.gcd(343, 119) = ",math.gcd(343, 119))
+
+#math constants
+print("math.e = ",math.e)
+print("math.inf = ",math.inf)
+print("math.nan = ",math.nan)
+print("math.pi = ",math.pi)
+print("math.tau = ",math.tau)
