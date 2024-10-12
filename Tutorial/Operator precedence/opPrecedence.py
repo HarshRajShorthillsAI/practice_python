9e9fb830e65eb31c729b0efa2873c120e004bfa8
HarshRajShorthillsAI <harsh.raj@shorthills.ai>
2024-10-12 11:06:19 +0530

    Commited for Operator Precedence

---
+++ Tutorial/Operator precedence/opPrecedence.py
@@ -0,0 +1,28 @@
+expr = 10 + 20 * 30
+
+print("3. 10 + 20 * 30 = ",expr)
+
+name = "Alex"
+age = 0
+
+if name == "Alex" or name == "John" and age >= 2:
+    print("8. Hello! Welcome", name)
+else:
+    print("8. Good bye!!")
+
+if (name == "Alex" or name == "John") and age >= 2:
+    print("13. Hello! Welcome.",name)
+else:
+    print("13. Good bye!!")
+
+print("18. ",100 / 10 * 10)
+
+print("20. ",5 - 2 + 3)
+
+print("22. ",5 - ( 2 + 3))
+
+print("24. ",2 ** 3 ** 2)
+
+expression = 100 + 200 / 10 - 3 * 10
+
+print("28. ",expression)
