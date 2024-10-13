4bfb8c3e248c1b2e9902d68f4933ef31b6165f4e
HarshRajShorthillsAI <harsh.raj@shorthills.ai>
2024-10-13 13:33:36 +0530

    Commited for Indentation in python

---
+++ Tutorial/Indentation in Python/indentation.py
@@ -0,0 +1,9 @@
+# Example of a function with proper indentation
+def greet(name):
+    if name:
+        print("Hello, " + name + "!")
+    else:
+        print("Hello, World!")
+
+# Call the function
+greet("Alice")
